b0385595783e71e00dac962aefba314a0b2cd510
Patrik Bajkusa <patrikb99@hotmail.com>
2024-04-04 19:50:15 +0200

    Added stripe

--- client/frontend/src/pages/ShoppingCart.tsx
+++ client/frontend/src/pages/ShoppingCart.tsx
@@ -1,6 +1,31 @@
 export const ShoppingCart = () => {
   const handleCheckout = () => {
-    console.log("Hello World");
+    fetch("http://localhost:3000/create-checkout-session", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        items: [
+          {
+            id: 1,
+            quantity: 2,
+          },
+          {
+            id: 2,
+            quantity: 1,
+          },
+        ],
+      }),
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+        return res.json().then((json) => Promise.reject(json));
+      })
+      .then(({ url }) => {
+        window.location = url;
+      })
+      .catch((e: any) => {
+        console.error(e.error);
+      });
   };
   return <button onClick={handleCheckout}>Checkout</button>;
 };
--- server/server.js
+++ server/server.js
@@ -1,6 +1,44 @@
 const express = require("express");
-const CookieSession = require("cookie-session")
-
+require("dotenv").config();
+const CookieSession = require("cookie-session");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+
+const storeItems = new Map([
+  [1, { priceInCents: 10000, name: "Lets go" }],
+  [2, { priceInCents: 20000, name: "Ehyo Lets go" }],
+]);
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: req.body.items.map((item) => {
+        const storeItem = storeItems.get(item.id);
+        return {
+          price_data: {
+            currency: "sek",
+            product_data: {
+              name: storeItem.name,
+            },
+            unit_amount: storeItem.priceInCents,
+          },
+          quantity: item.quantity,
+        };
+      }),
+      success_url: `${process.env.CLIENT_URL}/confirmedpayment`,
+      cancel_url: `${process.env.CLIENT_URL}/shoppingcart`,
+    });
+    res.json({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 
 app.listen(3000, () => console.log("Server is up n running.."));
